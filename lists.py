b1e04c933e350397d99b37434d0d78faac1c2622
moramirave <mmirave@mail.austral.edu.ar>
2025-05-04 18:39:43 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -20,9 +20,9 @@ def add_elements(list_to_add_elements):
 
 def is_empty(list_to_check):
     if len(list_to_check) == 0:
-        return "Esta vacia"
+        return True
     else:
-        return "No esta vacia"
+        return False
 
 def check_lists(list_to_compare1, list_to_compare2):
     lista1 = list_to_compare1[:] 
